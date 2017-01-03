bbb5262f9f5f1815f65d5f406d92d15d5f412921
Per-Olav S. Eikrem <perolaveikrem@hotmail.com>
2017-01-03 16:10:57 +0100

    Videoly
    ASP.NET MVC web project

---
+++ Videoly/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Videoly.Startup))]
+namespace Videoly
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
